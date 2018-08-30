2c26ed39bee96d21225f0a5ccc78359b8ae50fe2
SabithHaneef <sabith.th.07@gmail.com>
2018-08-30 13:37:33 +0530

    added express router to refactor

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const express = require('express');
 const app = express();
 
 const { accounts, users, writeJSON } = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,45 +20,8 @@ app.get('/', (req, res) => {
     });
 });
 
-app.get('/savings', (req, res) => {
-    res.render('account', {
-        account: accounts.savings
-    });
-});
-
-app.get('/checking', (req, res) => {
-    res.render('account', {
-        account: accounts.checking
-    });
-});
-
-app.get('/credit', (req, res) => {
-    res.render('account', {
-        account: accounts.credit
-    });
-});
-
-app.get('/transfer', (req, res) => {
-    res.render('transfer');
-});
-
-app.post('/transfer', (req, res) => {
-    accounts[req.body.from].balance = parseInt(accounts[req.body.from].balance) - parseInt(req.body.amount);
-    accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(req.body.amount);
-    writeJSON();
-    res.render('transfer', { message: 'Transfer Completed' });
-});
-
-app.get('/payment', (req, res) => {
-    res.render('payment', { account: accounts.credit });
-});
-
-app.post('/payment', (req, res) => {
-    accounts.credit.balance -= parseInt(req.body.amount);
-    accounts.credit.available += parseInt(req.body.amount, 10);
-    writeJSON();
-    res.render('payment', { message: 'Payment Successfull', account: accounts.credit });
-})
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/profile', (req, res) => {
     res.render('profile', {
